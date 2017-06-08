231ddad047717a10fca73c1c234e1044c328b78e
John Ferguson <johngeorgeferguson@gmail.com>
2017-06-08 12:26:17 +0100

    Update isPrime to use division test method - but not working for numbers only divisible by 5, probably because isPrime isn't calling its lamda recursively

--- pe3.rkt
+++ pe3.rkt
@@ -7,14 +7,14 @@
 
 (define (isPrime? number)
   (cond
-    [(equal? number 0) #f]
-    [(equal? number 1) #f]
-    [(equal? number 2) #t]
-    [(equal? number 3) #t]
-    [(equal? number 5) #t]
+    [(<= number 1) #f]
+    [(< number 3) #t]
     [(isFactor? number 2) #f]
     [(isFactor? number 3) #f]
-    [(isFactor? number 5) #f]
+    [(lambda (i n)
+      (cond
+        [(<= (* i i) n) (or (isFactor? n i) (isFactor? n (+ i 2))) #f]) 
+      5 number)]
     [else #t]))
 
 (define (isPrimeFactor? number testFactor)
@@ -24,24 +24,35 @@
     [(equal? testFactor 2) #t]
     [else #f]))
 
-(isPrime? 0)
-(isPrime? 1)
-(isPrime? 2)
-(isPrime? 3)
-(isPrime? 4)
-(isPrime? 5)
-(isPrime? 6)
-(isPrime? 7)
-(isPrime? 8)
-(isPrime? 9)
-(isPrime? 10)
-(isPrime? 11)
-(isPrime? 12)
-(isPrime? 13)
-(isPrime? 14)
-(isPrime? 15)
-(isPrime? 16)
-(isPrime? 17)
-(isPrime? 18)
-(isPrime? 19)
-(isPrime? 20)
+(define (testSuitePrimes start end)
+  (cond
+    [(isPrime? start) (printf "~a isPrime?~n" start)])
+  (cond
+    [(and (< start end)) (testSuitePrimes (+ 1 start) end)]))
+
+(displayln "Run tests...")
+(testSuitePrimes 1 229)
+
+
+; (printf "0 isPrime? ~a ~n" (isPrime? 0))
+; (printf "1 isPrime? (isPrime? 1)~n")
+; (isPrime? 1)
+; (isPrime? 2)
+; (isPrime? 3)
+; (isPrime? 4)
+; (isPrime? 5)
+; (isPrime? 6)
+; (isPrime? 7)
+; (isPrime? 8)
+; (isPrime? 9)
+; (isPrime? 10)
+; (isPrime? 11)
+; (isPrime? 12)
+; (isPrime? 13)
+; (isPrime? 14)
+; (isPrime? 15)
+; (isPrime? 16)
+; (isPrime? 17)
+; (isPrime? 18)
+; (isPrime? 19)
+; (isPrime? 20)
