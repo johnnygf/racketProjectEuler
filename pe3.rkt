9649ffdc435dee5b66454cff9b1278dc72f935a5
John Ferguson <johngeorgeferguson@gmail.com>
2017-08-10 13:52:46 +0100

    Project Euler 3 solved!

--- pe3.rkt
+++ pe3.rkt
@@ -8,21 +8,28 @@
 (define (isPrime? number)
   (cond
     [(<= number 1) #f]
-    [(< number 3) #t]
+    [(<= number 3) #t]
     [(isFactor? number 2) #f]
     [(isFactor? number 3) #f]
-    [(lambda (i n)
-      (cond
-        [(<= (* i i) n) (or (isFactor? n i) (isFactor? n (+ i 2))) #f]) 
-      5 number)]
-    [else #t]))
+    [else (isPrimeLoop number 5)]))
 
-(define (isPrimeFactor? number testFactor)
+(define (isPrimeLoop number i)
   (cond
-    [(equal? testFactor 0) #f]
-    [(equal? testFactor 1) #f]
-    [(equal? testFactor 2) #t]
-    [else #f]))
+    [(> (* i i) number) #t]
+    [else (cond
+      [(or (isFactor? number i) (isFactor? number (+ i 2))) #f]
+      [else (isPrimeLoop number (+ i 6))])]))
+
+(define (largestPrimeFactor number)
+  (cond
+    [(isPrime? number) number]
+    [else (largestPrimeFactorLoop number 2)]))
+
+(define (largestPrimeFactorLoop number testNumber)
+  (cond
+    [(isFactor? number testNumber) (largestPrimeFactor (/ number testNumber))]
+    [(isPrime? number) number]
+    [else (largestPrimeFactorLoop number (+ testNumber 1))]))
 
 (define (testSuitePrimes start end)
   (cond
@@ -30,29 +37,10 @@
   (cond
     [(and (< start end)) (testSuitePrimes (+ 1 start) end)]))
 
-(displayln "Run tests...")
-(testSuitePrimes 1 229)
-
-
-; (printf "0 isPrime? ~a ~n" (isPrime? 0))
-; (printf "1 isPrime? (isPrime? 1)~n")
-; (isPrime? 1)
-; (isPrime? 2)
-; (isPrime? 3)
-; (isPrime? 4)
-; (isPrime? 5)
-; (isPrime? 6)
-; (isPrime? 7)
-; (isPrime? 8)
-; (isPrime? 9)
-; (isPrime? 10)
-; (isPrime? 11)
-; (isPrime? 12)
-; (isPrime? 13)
-; (isPrime? 14)
-; (isPrime? 15)
-; (isPrime? 16)
-; (isPrime? 17)
-; (isPrime? 18)
-; (isPrime? 19)
-; (isPrime? 20)
+(displayln "")
+(displayln "")
+(printf "45: largetsPrimeFactor = ~a~n" (largestPrimeFactor 45))
+(displayln "")
+(printf "13195: largetsPrimeFactor = ~a~n" (largestPrimeFactor 13195))
+(displayln "")
+(printf "600851475143: largetsPrimeFactor = ~a~n" (largestPrimeFactor 600851475143))
